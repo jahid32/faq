b615dac5602a47811928c0e6c17d3f30535e7100
Mostafizur Rahman <mostafizur.rahman@sourcetop.com>
2014-09-15 20:26:00 +0600

    add markup

--- faq.js
+++ faq.js
@@ -1,6 +1,20 @@
-;function($){
-  $.fn.faq = function(){
-    
+;(function($){
+  $.fn.mrjsfaq = function(){
+    var $desc= this.children('.faq-description');
+    var $title=this.children('.faq-title');
+    $desc.first().css('display','block');
+    console.log($desc);
+
+
+
+    $title.on('click',function(){
+        if($title.next().$desc.is(':visible')){return;}
+        else{
+          $desc.slideUp();
+          $title.next().$desc.slideDown();
+        }
+    });
+    return this;
   };
 
 }(jQuery));
